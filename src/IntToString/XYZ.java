fe4336299b1c14924b79169ba4bf5724ba910bef
Nitish2 <nitishn886@gmail.com>
2017-06-20 14:18:37 +0530

    Assignment13.1

---
+++ src/IntToString/XYZ.java
@@ -0,0 +1,21 @@
+package IntToString;
+/**
+ * Here we are converting an int variable to String using an inbuilt function toString().
+ * @author Hp
+ *
+ */
+public class XYZ {
+
+	private static String String;
+
+	public static void main(String[] args) {
+		int a = 1;    // assigning value to 
+            
+		String num =String.valueOf(a);       // making a string object num and converting an int variable into string 
+		
+		System.out.println(" Int variable converted to string:" + num);     //prints the string value
+
+		
+	}
+
+}
